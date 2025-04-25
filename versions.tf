2806a978c005b8a15b6559ae0b5bd9ee05acbc4e
Alex Rembez <arembez@gmail.com>
2025-04-25 09:21:24 +0300

    Provider versions corrections

--- versions.tf
+++ versions.tf
@@ -3,11 +3,11 @@ terraform {
   required_providers {
     yandex = {
       source  = "yandex-cloud/yandex" # Yandex Cloud provider
-      version = ">= 0.130"
+      version = "~> 0.130"
     }
     aws = {
       source  = "hashicorp/aws" # AWS provider (used for Terraform lock table emulation)
-      version = ">= 5.44"
+      version = "~> 5.44"
     }
   }
 }
